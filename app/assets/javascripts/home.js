c22d58c0097c0a4f6e8811813dfad19d63a79cb6
細田来夢 <52363465+adwyaatd@users.noreply.github.com>
2020-03-04 19:32:15 +0900

    jQuery導入（読み込みの修正）、モーダル作成 仮想リクエスト

---
+++ app/assets/javascripts/home.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  $('li').on('click','#logout',function(){
+		$('#logout-modal').fadeIn();
+  });
+
+  $(function(){ // if document is ready
+    alert('jQuery is ready.')
+  });
+  
+  // jQuery稼働確認テスト用
+  // $("p").text("jQuery稼働テスト(稼働中)");
+  // $("#hoge").click(function(){
+  //   $("#hoge").css("color","red")
+  // });
+});
